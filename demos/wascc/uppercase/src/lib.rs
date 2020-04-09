9914ad0def338d77c8bca9e6c19b2cac87cba396
Brian Ketelsen <mail@bjk.fyi>
2020-04-09 11:21:25 -0400

    add wascc demo

---
+++ demos/wascc/uppercase/src/lib.rs
@@ -0,0 +1,39 @@
+extern crate wascc_actor as actor;
+
+#[macro_use]
+extern crate log;
+extern crate serde;
+extern crate wascc_codec;
+
+use actor::prelude::*;
+use serde::Serialize;
+use wascc_codec::serialize;
+
+actor_handlers! {
+    codec::http::OP_HANDLE_REQUEST => uppercase,
+    codec::core::OP_HEALTH_REQUEST => health
+}
+
+fn uppercase(
+    r: codec::http::Request
+) -> CallResult{
+    info!("Query String: {}", r.query_string);
+        let upper = UppercaseResponse {
+            original: r.query_string.to_string(),
+            uppercased: r.query_string.to_ascii_uppercase(),
+        };
+
+     Ok(serialize(codec::http::Response::json(upper, 200, "OK"))?)
+}
+
+fn health(
+    _req: codec::core::HealthRequest
+) -> ReceiveResult{
+    Ok(vec![])
+}
+
+#[derive(Serialize)]
+struct UppercaseResponse {
+    original: String,
+    uppercased: String,
+}
