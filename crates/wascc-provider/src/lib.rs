cb46ca083cfa4de6667675e4c3bab9b0cecec7bd
Ryan Levick <ryan.levick@gmail.com>
2020-04-09 14:06:56 +0200

    Statically link providers

--- crates/wascc-provider/src/lib.rs
+++ crates/wascc-provider/src/lib.rs
@@ -119,7 +119,7 @@ impl<S: ModuleStore + Send + Sync> WasccProvider<S> {
         let log_path = config.data_dir.join(LOG_DIR_NAME);
         tokio::fs::create_dir_all(&log_path).await?;
 
-        // wascc has native and porstable capabilities.
+        // wascc has native and protable capabilities.
         //
         // Native capabilities are either dynamic libraries (.so, .dylib, .dll)
         // or statically linked Rust libaries. If the native capabilty is a dynamic
@@ -138,6 +138,7 @@ impl<S: ModuleStore + Send + Sync> WasccProvider<S> {
             let http_provider = HttpServerProvider::new();
             let data = NativeCapability::from_instance(http_provider, None)
                 .map_err(|e| anyhow::anyhow!("Failed to instantiate HTTP capability: {}", e))?;
+
             cloned_host
                 .lock()
                 .unwrap()
