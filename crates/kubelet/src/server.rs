37ecdf919eae65b7945286905504a7c71385f622
kflansburg <kevin.flansburg@gmail.com>
2020-05-07 11:41:50 -0500

    cargo fmt

--- crates/kubelet/src/server.rs
+++ crates/kubelet/src/server.rs
@@ -19,12 +19,8 @@ pub async fn start_webserver<T: 'static + Provider + Send + Sync>(
     provider: Arc<T>,
     config: &ServerConfig,
 ) -> anyhow::Result<()> {
-    let health = warp::get()
-        .and(warp::path("healthz"))
-        .map(get_ping);
-    let ping = warp::get()
-        .and(warp::path::end())
-        .map(get_ping);
+    let health = warp::get().and(warp::path("healthz")).map(get_ping);
+    let ping = warp::get().and(warp::path::end()).map(get_ping);
 
     let logs_provider = provider.clone();
     let logs = warp::get()
