26d20ae4c938370bc0ed126d0a1f613cf876f339
Adam Reese <adam@reese.io>
2020-06-08 10:02:51 -0700

    fix(hack/bootstrap.sh): support gnu and BSD
    * Add LC_CTYPE to avoid `tr: Illegal byte sequence` error
    * Support gnu and BSD date commands
    * Refactor copy pasta

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -1,23 +1,28 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -euo pipefail
 
-token_id=$(< /dev/urandom tr -dc a-z0-9 | head -c${1:-6};echo;)
+export LC_CTYPE=C
+
+token_id="$(</dev/urandom tr -dc a-z0-9 | head -c "${1:-6}";echo;)"
 # This needs to use /dev/random so it is cryptographically safe
-token_secret=$(< /dev/random tr -dc a-z0-9 | head -c${1:-16};echo;)
+token_secret="$(</dev/random tr -dc a-z0-9 | head -c "${1:-16}";echo;)"
+
+# support gnu and BSD date command
+expiration=$(date -u "+%Y-%m-%dT%H:%M:%SZ" --date "1 hour" 2>/dev/null ||
+  date -v1H -u "+%Y-%m-%dT%H:%M:%SZ" 2>/dev/null)
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
-  name: bootstrap-token-$(echo -n $token_id)
+  name: bootstrap-token-${token_id}
   namespace: kube-system
 type: bootstrap.kubernetes.io/token
 stringData:
   auth-extra-groups: system:bootstrappers:kubeadm:default-node-token
-  expiration: $(date -u +"%Y-%m-%dT%H:%M:%SZ" --date "1 hour")
-  token-id: $(echo -n $token_id)
-  token-secret: $(echo -n $token_secret)
+  expiration: ${expiration}
+  token-id: ${token_id}
+  token-secret: ${token_secret}
   usage-bootstrap-authentication: "true"
   usage-bootstrap-signing: "true"
 EOF
@@ -25,43 +30,48 @@ EOF
 # Helpful script taken from the armory docs: https://docs.armory.io/spinnaker-install-admin-guides/manual-service-account/
 # and modified to suit our needs
 
+config_dir=${CONFIG_DIR:-$HOME/.krustlet/config}
+mkdir -p "${config_dir}"
+
 CONTEXT=$(kubectl config current-context)
 NAMESPACE=kube-system
-
 NEW_CONTEXT=tls-bootstrap-token-user@kubernetes
-config_dir=${CONFIG_DIR:-$HOME/.krustlet/config}
 file_name=${FILE_NAME:-bootstrap.conf}
 KUBECONFIG_FILE="${config_dir}/${file_name}"
 TOKEN_USER=tls-bootstrap-token-user
+TOKEN="${token_id}.${token_secret}"
 
-mkdir -p ${config_dir}
-
-TOKEN=$(echo -n "${token_id}.${token_secret}")
+# Cleanup tmp files
+trap 'rm -f ${KUBECONFIG_FILE}.{full.tmp,tmp}' EXIT
 
 # Create dedicated kubeconfig
+
 # Create a full copy
-kubectl config view --raw > ${KUBECONFIG_FILE}.full.tmp
+kubectl config view --raw >"${KUBECONFIG_FILE}.full.tmp"
+
 # Switch working context to correct context
-kubectl --kubeconfig ${KUBECONFIG_FILE}.full.tmp config use-context ${CONTEXT}
+kubectl --kubeconfig "${KUBECONFIG_FILE}.full.tmp" config use-context "${CONTEXT}"
+
 # Minify
-kubectl --kubeconfig ${KUBECONFIG_FILE}.full.tmp \
-  config view --flatten --minify > ${KUBECONFIG_FILE}.tmp
+kubectl --kubeconfig "${KUBECONFIG_FILE}.full.tmp" \
+  config view --flatten --minify >"${KUBECONFIG_FILE}.tmp"
+
 # Rename context
-kubectl config --kubeconfig ${KUBECONFIG_FILE}.tmp \
-  rename-context ${CONTEXT} ${NEW_CONTEXT}
+kubectl config --kubeconfig "${KUBECONFIG_FILE}.tmp" \
+  rename-context "${CONTEXT}" "${NEW_CONTEXT}"
+
 # Create token user
-kubectl config --kubeconfig ${KUBECONFIG_FILE}.tmp \
-  set-credentials ${TOKEN_USER} \
-  --token ${TOKEN}
+kubectl config --kubeconfig "${KUBECONFIG_FILE}.tmp" \
+  set-credentials "${TOKEN_USER}" --token "${TOKEN}"
+
 # Set context to use token user
-kubectl config --kubeconfig ${KUBECONFIG_FILE}.tmp \
-  set-context ${NEW_CONTEXT} --user ${TOKEN_USER}
+kubectl config --kubeconfig "${KUBECONFIG_FILE}.tmp" \
+  set-context "${NEW_CONTEXT}" --user "${TOKEN_USER}"
+
 # Set context to correct namespace
-kubectl config --kubeconfig ${KUBECONFIG_FILE}.tmp \
-  set-context ${NEW_CONTEXT} --namespace ${NAMESPACE}
+kubectl config --kubeconfig "${KUBECONFIG_FILE}.tmp" \
+  set-context "${NEW_CONTEXT}" --namespace "${NAMESPACE}"
+
 # Flatten/minify kubeconfig
-kubectl config --kubeconfig ${KUBECONFIG_FILE}.tmp \
-  view --flatten --minify > ${KUBECONFIG_FILE}
-# Remove tmp
-rm ${KUBECONFIG_FILE}.full.tmp
-rm ${KUBECONFIG_FILE}.tmp
+kubectl config --kubeconfig "${KUBECONFIG_FILE}.tmp" \
+  view --flatten --minify >"${KUBECONFIG_FILE}"
