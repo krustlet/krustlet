45159fb0cbbe611e296e27b011428b3917e743c1
itowlson <ivan.towlson@microsoft.com>
2020-06-15 17:23:35 +1200

    Convert pull policy string to enum

--- crates/kubelet/src/module_store.rs
+++ crates/kubelet/src/module_store.rs
@@ -12,6 +12,39 @@ use std::convert::TryFrom;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
+/// Specifies how the store should check for module updates
+pub enum ModulePullPolicy {
+    /// Always return the module as it currently appears in the
+    /// upstream registry
+    Always,
+    /// Return the module as it is currently cached in the local store if
+    /// present; fetch it from the upstream registry only if it it not
+    /// present in the local store
+    IfNotPresent,
+    /// Never fetch the module from the upstream registry; if it is not
+    /// available locally then return an error
+    Never,
+}
+
+impl ModulePullPolicy {
+    /// Parses a module pull policy from a Kubernetes ImagePullPolicy string
+    pub fn parse(name: Option<String>) -> anyhow::Result<Option<ModulePullPolicy>> {
+        match name {
+            None => Ok(None),
+            Some(n) => ModulePullPolicy::parse_str(&n[..]),
+        }
+    }
+
+    fn parse_str(name: &str) -> anyhow::Result<Option<ModulePullPolicy>> {
+        match name {
+            "Always" => Ok(Some(Self::Always)),
+            "IfNotPresent" => Ok(Some(Self::IfNotPresent)),
+            "Never" => Ok(Some(Self::Never)),
+            other => Err(anyhow::anyhow!("unrecognized pull policy {}", other)),
+        }
+    }
+}
+
 /// A store of container modules.
 ///
 /// This provides the ability to get a module's bytes given an image [`Reference`].
@@ -78,21 +111,21 @@ pub trait ModuleStore {
     async fn get(
         &self,
         image_ref: &Reference,
-        pull_policy: Option<String>,
+        pull_policy: Option<ModulePullPolicy>,
     ) -> anyhow::Result<Vec<u8>> {
         // Specification from https://kubernetes.io/docs/concepts/configuration/overview/#container-images):
         let effective_pull_policy = pull_policy.unwrap_or(match image_ref.tag() {
-            Some("latest") | None => "Always".to_owned(),
-            _ => "IfNotPresent".to_owned(),
+            Some("latest") | None => ModulePullPolicy::Always,
+            _ => ModulePullPolicy::IfNotPresent,
         });
 
-        match effective_pull_policy.as_str() {
-            "IfNotPresent" => {
+        match effective_pull_policy {
+            ModulePullPolicy::IfNotPresent => {
                 if !self.is_present_by_ref(image_ref).await {
                     self.pull(image_ref).await?
                 }
             }
-            "Always" => {
+            ModulePullPolicy::Always => {
                 if !self
                     .is_present_by_digest(self.resolve_registry_digest(image_ref).await?)
                     .await
@@ -100,13 +133,7 @@ pub trait ModuleStore {
                     self.pull(image_ref).await?
                 }
             }
-            "Never" => (),
-            _ => {
-                return Err(anyhow::anyhow!(
-                    "Unrecognized image pull policy {}",
-                    effective_pull_policy
-                ))
-            }
+            ModulePullPolicy::Never => (),
         };
 
         self.get_local(image_ref).await
@@ -132,7 +159,7 @@ pub trait ModuleStore {
                 .clone()
                 .expect("FATAL ERROR: container must have an image");
             let reference = Reference::try_from(image).unwrap();
-            let pull_policy = container.image_pull_policy.clone();
+            let pull_policy = ModulePullPolicy::parse(container.image_pull_policy.clone()).unwrap();
             async move {
                 Ok((
                     container.name.clone(),
@@ -207,11 +234,12 @@ impl<C: ImageClient + Send> ModuleStore for FileModuleStore<C> {
         path.exists()
     }
 
-    async fn is_present_by_digest(&self, digest: String) -> bool {
-        panic!(format!(
-            "TODO: is_present_by_digest not implemented while looking for {}",
-            digest
-        ));
+    async fn is_present_by_digest(&self, _digest: String) -> bool {
+        // panic!(format!(
+        //     "TODO: is_present_by_digest not implemented while looking for {}",
+        //     digest
+        // ));
+        todo!("is_present_by_digest not implemented");
     }
 
     async fn pull(&self, image_ref: &Reference) -> anyhow::Result<()> {
