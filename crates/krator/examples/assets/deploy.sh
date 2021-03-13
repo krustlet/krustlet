a9598e3b62885e4d29bbe07ec1c547729e750631
kflansburg <kevin.flansburg@gmail.com>
2021-03-13 14:09:56 -0600

    Admission webhook tracing.

--- crates/krator/examples/assets/deploy.sh
+++ crates/krator/examples/assets/deploy.sh
@@ -6,11 +6,27 @@ openssl genrsa -out ca.key 2048
 # Create CA Cert
 openssl req -new -key ca.key -x509 -out ca.crt -days 3650 -subj "/CN=ca"
 
+cat << EOF > req.conf
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = moose-admission-webhook
+DNS.2 = moose-admission-webhook.default
+DNS.3 = moose-admission-webhook.default.svc
+EOF
+
 # Create Server Key and Signing Request
-openssl req -new -nodes -newkey rsa:2048 -keyout server.key -out server.req -batch -subj "/CN=moose-admission-webhook.default.svc" 
+openssl req -new -nodes -newkey rsa:2048 -keyout server.key -out server.req -batch -config req.conf -subj "/"
 
 # Create Signed Server Cert
-openssl x509 -req -in server.req -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -sha256
+openssl x509 -req -in server.req -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -sha256 -extensions v3_req --extfile req.conf
 
 case `uname` in
         Darwin)
