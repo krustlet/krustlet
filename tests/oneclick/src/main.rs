b1723ca93e1a4889ab9437574d1275d6367abfde
Kevin Flansburg <kevin.flansburg@gmail.com>
2021-04-18 17:34:34 -0500

    log level

--- tests/oneclick/src/main.rs
+++ tests/oneclick/src/main.rs
@@ -287,7 +287,7 @@ fn launch_kubelet(
         .env("KUBECONFIG", kubeconfig)
         .env(
             "RUST_LOG",
-            "wasi_provider=debug,main=debug,krator::state=debug,kubelet::container::state=debug,kubelet::webserver=debug,warp=debug,kubelet::node=debug",
+            "wasi_provider=debug,main=debug,krator::state=debug,kubelet::container::state=debug,kubelet::webserver=debug,warp=debug,kubelet::node=debug,kubelet::config=debug",
         )
         .stdout(std::process::Stdio::piped())
         .stderr(stderr)
